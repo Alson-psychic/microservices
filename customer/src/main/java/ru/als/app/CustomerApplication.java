1acb64226181f42afb5d40ccced2bcb824db7cc7
alson <filsalouna@gmail.com>
2024-11-29 02:11:53 +0300

    Added zipkin

--- customer/src/main/java/ru/als/app/CustomerApplication.java
+++ customer/src/main/java/ru/als/app/CustomerApplication.java
@@ -6,12 +6,22 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ru.als.app.customer"
+        }
+)
 @EnableEurekaClient
-@EnableFeignClients
-@ComponentScan(basePackages = "ru.als.app")
+@EnableFeignClients(
+        basePackages = "ru.als.app.clients"
+)
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
