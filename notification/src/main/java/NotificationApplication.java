47e94cd6e9d39d76e05e9eea682dff34f40adaa3
alson <filsalouna@gmail.com>
2024-11-29 04:53:09 +0300

    Added Rabbitmq

--- notification/src/main/java/NotificationApplication.java
+++ notification/src/main/java/NotificationApplication.java
@@ -1,13 +1,18 @@
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import ru.als.app.RabbitMQMessageProducer;
+import ru.als.app.config.NotificationConfig;
 
 
 @SpringBootApplication(
         scanBasePackages = {
-                "com.amigoscode.notification",
-                "com.amigoscode.amqp",
+                "ru.als.app.notification",
+                "ru.als.app.amqp",
+                "ru.als.app"
         }
 )
 @PropertySources({
@@ -16,5 +21,17 @@ import org.springframework.context.annotation.PropertySources;
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
     }
 }
